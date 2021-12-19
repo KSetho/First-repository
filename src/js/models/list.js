08d1504433669bc0ac4d3ffdf147afc1a6338967
Seth <sethaboagye243@gmail.com>
2021-12-19 21:07:16 +0000

    Add existing file

--- src/js/models/list.js
+++ src/js/models/list.js
@@ -1,4 +1,4 @@
-import * as uniqid from 'uniqid';
+import uniqid from 'uniqid';
 
 export default class List {
     constructor() {
@@ -19,6 +19,8 @@ export default class List {
     deleteItem(id) {
         const index = this.items.findIndex(el => el.id === id);
         //[2,4,8] splice(1,1) --> return 4,original array is [2,8]
+        //[2,4,8,10] splice(1,2) --> return [4,8]original array is [2,10] the last number in the box determines how many elements you need take
+        //[2,4,8] slice(1,1) --> return 4,original array is [2,8] the last number in the box is the end index
         this.item.splice(index,1);
     }
 
